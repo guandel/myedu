4a1e5c28737e0910b66db9c87b061af1d38371f7
关德朗 <1765529359@qq.com>
2019-04-19 12:00:34 +0800

    新增list查询

---
+++ day2/list_type.py
@@ -0,0 +1,10 @@
+def list_set(a):
+    print(a[3])
+    print(a[0:3])
+    print(a[2:4])
+    print(a[-3])
+
+
+if __name__ == '__main__':
+    alist = [1, 'a', 'l', 'hao', 'q','nihao']
+    list_set(alist)
